c3c1a4f8878bf8554569e3e1ee0563c28d26f7ad
Darius Tanz <scratchticketseb@gmail.com>
2023-06-23 01:50:30 +0000

    Jun 22, 2023, 9:50 PM

--- plots-finance/js/interaction.js
+++ plots-finance/js/interaction.js
@@ -31,6 +31,7 @@ async function loginWithEth(){
 
 async function Update(){
     document.getElementById("EthCount").innerText = document.getElementById("input").value;
+    document.getElementById("VLND").innerText = document.getElementById("input").value;
 }
 
 async function pay(){
