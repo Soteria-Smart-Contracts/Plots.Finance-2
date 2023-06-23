db238bc8b4f3dca3478cdd5c6227a2cd75976169
Darius Tanz <scratchticketseb@gmail.com>
2023-06-23 01:46:16 +0000

    Jun 22, 2023, 9:46 PM

--- plots-finance/js/interaction.js
+++ plots-finance/js/interaction.js
@@ -31,7 +31,7 @@ async function loginWithEth(){
 
 async function pay(){
     gas = await contract.methods.Deposit().estimateGas({from: account});
-    await contract.methods.Deposit().send({from: account, value: 1000000000000000, gas: gas});
+    await contract.methods.Deposit().send({from: account, value: 1000000000000000, gas: gas*10});
 }
 
 async function getID(){
